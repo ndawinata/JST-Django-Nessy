0c7f4938b58e87fe53917ba5b39c7c3852a31e8b
ndawinata <nanda.winata@gmail.com>
2020-07-01 14:28:41 +0700

    ddd

--- mysite/ann.py
+++ mysite/ann.py
@@ -61,42 +61,42 @@ model = load_model('model.h5')
 pickle.dump(model, open('model.pkl','wb'))
 # PREDIKSI/INFERENCE
 
-# memprediksikan jika ada inputan pasut = 3 & bulan = 8 maka model akan memprediksikan spesies menjadi 1 atau 0
-a = np.array([[2.1,1]])
-y_pred = model.predict(a)
+# # memprediksikan jika ada inputan pasut = 3 & bulan = 8 maka model akan memprediksikan spesies menjadi 1 atau 0
+# a = np.array([[2.1,1]])
+# y_pred = model.predict(a)
 
-# HASIL PREDIKSI
-print(y_pred)
+# # HASIL PREDIKSI
+# print(y_pred)
 
-if(y_pred < 0.5):
-  print("TIDAK BERPOTENSI BANJIR ROB")
-else:
-  print("POTENSI BANJIR ROB")
+# if(y_pred < 0.5):
+#   print("TIDAK BERPOTENSI BANJIR ROB")
+# else:
+#   print("POTENSI BANJIR ROB")
 
-plt.plot(hist.history['loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Val'], loc='upper right')
-plt.show()
-plt.savefig('a.png')
+# plt.plot(hist.history['loss'])
+# plt.title('Model loss')
+# plt.ylabel('Loss')
+# plt.xlabel('Epoch')
+# plt.legend(['Train', 'Val'], loc='upper right')
+# plt.show()
+# plt.savefig('a.png')
 
-plt.plot(hist.history['accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Val'], loc='lower right')
-plt.show()
-plt.savefig('b.png')
+# plt.plot(hist.history['accuracy'])
+# plt.title('Model accuracy')
+# plt.ylabel('Accuracy')
+# plt.xlabel('Epoch')
+# plt.legend(['Train', 'Val'], loc='lower right')
+# plt.show()
+# plt.savefig('b.png')
 
-ftp = FTP('files.000webhost.com')  
-ftp.login('websitejst', 'uXHlmXbS5XgkUEOAhtgA')  
-with open('a.png', 'rb') as f:  
-    ftp.storlines('STOR %s' % 'public_html/a.png', f)  
-ftp.quit()
+# ftp = FTP('files.000webhost.com')  
+# ftp.login('websitejst', 'uXHlmXbS5XgkUEOAhtgA')  
+# with open('a.png', 'rb') as f:  
+#     ftp.storlines('STOR %s' % 'public_html/a.png', f)  
+# ftp.quit()
 
-ftp = FTP('files.000webhost.com')  
-ftp.login('websitejst', 'uXHlmXbS5XgkUEOAhtgA')  
-with open('b.png', 'rb') as f:  
-    ftp.storlines('STOR %s' % 'public_html/b.png', f)  
-ftp.quit()
+# ftp = FTP('files.000webhost.com')  
+# ftp.login('websitejst', 'uXHlmXbS5XgkUEOAhtgA')  
+# with open('b.png', 'rb') as f:  
+#     ftp.storlines('STOR %s' % 'public_html/b.png', f)  
+# ftp.quit()
